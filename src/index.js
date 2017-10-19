3d094a7ba1a73858ba98b50059c6c8ae06c51bb3
Aibol Kussain <Aibol_Kussain@epam.com>
2017-10-19 14:29:17 +0300

    wepback: Output management

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import lodash from 'lodash';
-import './style.css';
-import Icon from './icon.png';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = lodash.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  var myIcon = new Image();
-  myIcon.src = Icon;
+  btn.innerHTML = 'Click me and the check the console';
+  btn.onclick = printMe;
 
-  element.appendChild(myIcon);
+  element.appendChild(btn);
 
   return element;
 }
